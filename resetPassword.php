681f3de53af5ea0e3625329fcc82774b62e4edd1
zerraxdevnew <destiny.gaming.don@gmail.com>
2017-07-01 16:48:54 +0200

    Update resetPassword.php
    If action if isset get error 
    now it good

--- resetPassword.php
+++ resetPassword.php
@@ -90,13 +90,16 @@ require('layout/header.php');
 					}
 
 					//check the action
-					switch ($_GET['action']) {
-						case 'active':
-							echo "<h2 class='bg-success'>Your account is now active you may now log in.</h2>";
-							break;
-						case 'reset':
-							echo "<h2 class='bg-success'>Please check your inbox for a reset link.</h2>";
-							break;
+					if (isset($_GET['action']))
+					{
+						switch ($_GET['action']) {
+							case 'active':
+								echo "<h2 class='bg-success'>Your account is now active you may now log in.</h2>";
+								break;
+							case 'reset':
+								echo "<h2 class='bg-success'>Please check your inbox for a reset link.</h2>";
+								break;
+						}
 					}
 					?>
 
